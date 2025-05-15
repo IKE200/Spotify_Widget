e2550e0eb6fd646ea897076df3fdba721615224e
David <David@Bernhardt-nidderau.de>
2025-05-15 21:40:57 +0200

    testing

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   requestAnimationFrame(() => {
     const wrapper = document.getElementById("track-wrapper");
-    const threshold = 5; // pixels of buffer
+    const threshold = 500; // pixels of buffer
     if (wrapper.scrollWidth > trackContainer.clientWidth + threshold) {
       trackContainer.classList.add("scroll");
     } else {
