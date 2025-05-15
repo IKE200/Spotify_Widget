225c19741b5e56247fff20a0b3f894cc45bcc15a
David <David@Bernhardt-nidderau.de>
2025-05-15 21:44:49 +0200

    testing

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const data = {
-  track: "A Long Song Title That might need Scrolling",
+  track: "A Song Title That might need Scrolling",
   artist: "Cool Artist",
   cover: "https://i.scdn.co/image/ab67616d00001e02a0354b0310d9a6b3b3b64dd3"
 };
