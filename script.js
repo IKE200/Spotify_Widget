f81a7e69fe663b4fa17e46de03aef2cb81af531b
David <David@Bernhardt-nidderau.de>
2025-05-15 21:19:57 +0200

    testing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Example track info – replace with your API or integration
 const data = {
-  track: "A Very Very Very Long Song Title That Needs Scrolling",
+  track: "A short title",
   artist: "Cool Artist",
   cover: "https://i.scdn.co/image/ab67616d00001e02a0354b0310d9a6b3b3b64dd3"
 };
